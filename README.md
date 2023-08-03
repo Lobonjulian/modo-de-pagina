09828661d0bc80e2dd051be5b16024a978b1459d
Julian Lobon <julitolos6@gmail.com>
2023-08-02 20:06:43 -0500

    agregando color

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# modo-de-pagina
-Creando una pagina en la cual por medio de botones se cambia el fondo de pantalla 
+# Modo de Pagina
+Creando una pagina en la cual por en la cual hay 3 botones que cambian el fondo de pantalla 
+
+## 🛠 Skills
+Javascript, HTML, CSS...
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://katherineoelsner.com/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/julian-aguilar-/)
+
+
+![Logo](/imagenes/default-monochrome-black.svg)
+
+💖Like my work?
+
+📜 License
+[MIT](https://choosealicense.com/licenses/mit/)
--- indes.js
+++ indes.js
@@ -1,16 +1,23 @@
+/**
+ * The function `selecionarModo` changes the background color, background image, and title of an
+ * element based on the selected mode.
+ * @param modo - The parameter "modo" is a string that represents the mode to be selected. It can have
+ * three possible values: "claro" (light mode), "degradado" (gradient mode), or any other value (dark
+ * mode).
+ */
 function selecionarModo(modo) {
     
     let color = "black";
     let imagen = "claro";
     let titulo = "oscuro";
-
+    let colorImagen = ""
 
     if( modo === "claro"){
         color = "white";
         imagen = "claro";
         titulo = "Modo Claro";  
     } else if( modo === "degradado" ){
-        color = "#eb8a68";
+        colorImagen = "linear-gradient(to right top, #90867a, #a18877, #b48878, #c88780, #d98690, #e18294, #e97f99, #f96b82, #fb6471, #fb5f5f)";
         imagen = "claro";
         titulo = "Modo Degradado"; 
     }else {
@@ -18,6 +25,7 @@ function selecionarModo(modo) {
         imagen = "claro";
         titulo = "Modo Oscuro"; 
     }
-    document.getElementById("tema").style.backgroundColor = color;
+    document.getElementById("tema").style.backgroundColor = color ;
+    document.getElementById("tema").style.backgroundImage = colorImagen;
     document.getElementById("titulo").innerHTML = titulo;
 }
